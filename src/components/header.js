6dd84006728dfde89bea4994c77d024c628a0a8e
Ayush Bajracharya <realayush@gmail.com>
2021-04-20 22:37:06 +0545

    intro to class based components, a glance at destructring

--- src/components/header.js
+++ src/components/header.js
@@ -1,22 +1,12 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default function Header() {
-    const getYear = () =>{
-        const newDate = new Date();
-        return newDate.getFullYear();
+export default class Header extends Component {
+    render() {
+        return (
+            <header>
+                <div>logo</div>
+                <input type = "text" />
+            </header>
+        )
     }
-    const user = {
-        name: 'Ayush',
-        lastname: 'Bajracharya',
-        age: 26
-    }
-    return (
-        <div>
-           The date is {getYear()}
-           <div>name is  {user.name}</div>
-           <div>lastname is  {user.lastname}</div>
-           <div>age is  {user.age}</div>
-           
-        </div>
-    )
 }
