2fd65368417ccf7ba51be8a738967b63bba2b793
Ayush Bajracharya <realayush@gmail.com>
2021-04-20 22:31:21 +0545

    using dynamic data

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
 export default function Header() {
+    const getYear = () =>{
+        const newDate = new Date();
+        return newDate.getFullYear();
+    }
+    const user = {
+        name: 'Ayush',
+        lastname: 'Bajracharya',
+        age: 26
+    }
     return (
         <div>
-            header
+           The date is {getYear()}
+           <div>name is  {user.name}</div>
+           <div>lastname is  {user.lastname}</div>
+           <div>age is  {user.age}</div>
+           
         </div>
     )
 }
