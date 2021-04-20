451a4853b975297cc2766870d25869f57f1a8aab
Ayush Bajracharya <realayush@gmail.com>
2021-04-20 22:40:12 +0545

    8. inline styling

--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,18 @@ import React, { Component } from 'react'
 
 export default class Header extends Component {
     render() {
+        let styles = {
+            header:{
+                background: '#03a9f4',
+            },
+            logo:{
+                color: '#fff',
+                textAlign: 'center'
+            }
+        }
         return (
-            <header>
-                <div>logo</div>
+            <header style = {styles.header}>
+                <div style = {styles.logo}>logo</div>
                 <input type = "text" />
             </header>
         )
