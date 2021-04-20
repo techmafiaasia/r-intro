50e980d95f5c49b06e8a0f0515674114a9a9439d
Ayush Bajracharya <realayush@gmail.com>
2021-04-20 23:23:15 +0545

    glance at the state

--- src/components/header.js
+++ src/components/header.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react'
 
 export default class Header extends Component {
 
-    inputChangeHandler(e){
+    state = {
+        title: 'the keywords are: ',
+        keywords: 'hello'
+    }
+
+    inputChangeHandler = (e) => {
         console.log(e.target.value);
+        this.setState({
+            keywords: e.target.value
+        })
     }
     render() {
-      
+        console.log(this.state.keywords)
         return (
             <header>
                 <div className = "logo" onClick = {() => console.log('inline onclick function')}>logo</div>
                 <input type = "text" onChange = {this.inputChangeHandler} />
+                <div>{this.state.title}</div>
+                <div>{this.state.keywords}</div>
             </header>
         )
     }
