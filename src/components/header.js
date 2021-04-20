70b077d5acb37250d1bb8dd57ac46f5fb8a19cc4
Ayush Bajracharya <realayush@gmail.com>
2021-04-20 22:44:24 +0545

    8. adding styles by linking index.html to styles.css inside public folder

--- src/components/header.js
+++ src/components/header.js
@@ -2,18 +2,10 @@ import React, { Component } from 'react'
 
 export default class Header extends Component {
     render() {
-        let styles = {
-            header:{
-                background: '#03a9f4',
-            },
-            logo:{
-                color: '#fff',
-                textAlign: 'center'
-            }
-        }
+      
         return (
-            <header style = {styles.header}>
-                <div style = {styles.logo}>logo</div>
+            <header>
+                <div className = "logo">logo</div>
                 <input type = "text" />
             </header>
         )
